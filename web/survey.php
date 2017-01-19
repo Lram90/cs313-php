1271f74ae55229224e0ab4367535a34b48ff7197
Landon Mecham <mec15001@byui.edu>
2017-01-19 15:32:25 -0700

    Added first two prove_03 files

---
+++ web/survey.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Answers</title>
+	<!--<link rel="stylesheet" type="text/css" href="survey.css">-->
+	<!--<script type="text/javascript" src="week07.js"></script>-->
+</head>
+<body>
+	<h1>Internet Usage Answers</h1>
+	<h2>CS 313 Week 3 Prove Assignment</h2>
+	<?php
+    $often = $_POST["often"];
+	echo "<h3>How often?<h3>";
+	echo $often;
+	?>
+</body>
+</html>
+  
