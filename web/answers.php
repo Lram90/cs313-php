db918bf5a25053e632baaa599f6037bcd4f46615
Landon Mecham <mec15001@byui.edu>
2017-01-19 19:56:30 -0700

    survey on php page, 1st attempt session variables

---
+++ web/answers.php
@@ -0,0 +1,118 @@
+<?php
+	$_SESSION['submitted'] = False;
+
+	if (isset($_POST['submit'])){
+		$_SESSION['submitted'] = True;
+	}
+?>
+
+<html>
+<head>
+	<title>Answers</title>
+	<!--<link rel="stylesheet" type="text/css" href="survey.css">-->
+	<!--<script type="text/javascript" src="week07.js"></script>-->
+</head>
+<body>
+	<h1>Internet Usage Answers</h1>
+	<h2>CS 313 Week 3 Prove Assignment</h2>
+	<?php
+	$often = $_POST["often"] . "\n";
+	$use = $_POST["use"] . "\n";
+	$device = $_POST["device"] . "\n";
+	$nav = $_POST["nav"] . "\n";
+	$answers = fopen("survey.txt", "a") or die ("Unable to open file!");
+	
+	fwrite($answers, $often);
+	fwrite($answers, $use);
+	fwrite($answers, $device);
+	fwrite($answers, $nav);
+	fclose($answers);
+    ?>
+<!--Separated php for easier understanding (for me)-->
+<?php
+	$myfile = fopen("survey.txt", "r") or die("Unable to open file!");
+    	$rarely = $sometimes = $often = $allTheTime = 0;
+		$socialMedia = $playingGames = $shop = $watch = $research = 0;
+		$desktop = $laptop = $tablet = $phone = $console = 0;
+		$tabs = $search = $links = 0;
+		
+	while(!feof($myfile)){
+		$current = fgets($myfile);
+		switch($current)
+		{
+			case "rarely\n":
+			$rarely++;
+			break;
+			case "sometimes\n":
+			$sometimes++;
+			break;
+			case "often\n":
+			$often++;
+			break;
+			case "All The Time\n":
+			$allTheTime++;
+			break;
+			case "Social Media\n":
+			$socialMedia++;
+			break;
+			case "Playing Games\n":
+			$playingGames++;
+			break;
+			case "Shop\n":
+			$shop++;
+			break;
+			case "Watching Videos\n":
+			$watch++;
+			break;
+			case "Research\n":
+			$research++;
+			break;
+			case "Desktop\n":
+			$desktop++;
+			break;
+			case "Laptop\n":
+			$laptop++;
+			break;
+			case "Tablet\n":
+			$tablet++;
+			break;
+			case "Phone\n":
+			$phone++;
+			break;
+			case "Game Console\n":
+			$console++;
+			break;
+			case "tabs\n":
+			$tabs++;
+			break;
+			case "search\n":
+			$search++;
+			break;
+			case "links\n":
+			$links++;
+			break;
+			default:
+			break;
+		}
+	}
+    fclose($myfile);
+	
+	echo "<p>How often do you use the internet?</p><ul><li>Rarely:   " . $rarely
+	     . "</li><li>Sometimes:   " . $sometimes . "</li><li>Often:   " . $often
+		 . "</li><li> All The Time:   ". $allTheTime . "</li></ul>";
+	
+	echo "<p>What do you do most online?</p><ul><li>Use Social Media:   " . $socialMedia
+	     . "</li><li>Play Video Games:   " . $playingGames . "</li><li>Shop:   " . $shop
+		 . "</li><li>Watch Videos:   ". $watch . "</li><li>Research: " . $research . "</li></ul>";
+	
+	echo "<p>What device are you usually online with?</p><ul><li>Desktop:   " . $desktop
+	     . "</li><li>Laptop:   " . $laptop . "</li><li>Tablet:   " . $tablet
+		 . "</li><li> Smart Phone:   ". $phone . "</li><li>Video Game Console: " . $console . "</li></ul>";
+
+	echo "<p>How do you usually navigate a web-page?</p><ul><li>Tabs On the top:   " . $tabs . "</li><li>Search Bar: " . $search
+		 . "</li><li>Links in the page:   ". $links . "</li></ul>";	 
+	?>
+	
+</body>
+</html>
+  
--- web/survey.php
+++ web/survey.php
@@ -1,114 +1,56 @@
+<?php 
+	session_start();
+	if ($_SESSION['submitted'] === True){
+		header('location:answers.php');
+	}
+?>
+
+<!doctype html>
 <html>
 <head>
-	<title>Answers</title>
+	<title>CS 313 Prove 03</title>
 	<!--<link rel="stylesheet" type="text/css" href="survey.css">-->
 	<!--<script type="text/javascript" src="week07.js"></script>-->
 </head>
 <body>
-	<h1>Internet Usage Answers</h1>
+	<h1>Internet Usage Survey</h1>
 	<h2>CS 313 Week 3 Prove Assignment</h2>
-	<?php
-	$often = $_POST["often"] . "\n";
-	$use = $_POST["use"] . "\n";
-	$device = $_POST["device"] . "\n";
-	$nav = $_POST["nav"] . "\n";
-	$answers = fopen("survey.txt", "a") or die ("Unable to open file!");
 	
-	fwrite($answers, $often);
-	//fwrite($answers, "\n");
-	fwrite($answers, $use);
-	//fwrite($answers, "\n");
-	fwrite($answers, $device);
-	//fwrite($answers, "\n");
-	fwrite($answers, $nav);
-	//fwrite($answers, "\n");
-	fclose($answers);
-    ?>
-<!--Separated php for easier understanding (for me)-->
-<?php
-	$myfile = fopen("survey.txt", "r") or die("Unable to open file!");
-    	$rarely = $sometimes = $often = $allTheTime = 0;
-		$socialMedia = $playingGames = $shop = $watch = $research = 0;
-		$desktop = $laptop = $tablet = $phone = $console = 0;
-		$tabs = $search = $links = 0;
+	<form action="answers.php" method="post">
+		How often do you use the internet?<br>
+		<input type="radio" name="often" value="rarely" required="required">Rarely Ever<br>
+		<input type="radio" name="often" value="sometimes" required="required">Sometimes<br>
+		<input type="radio" name="often" value="often" required="required">Often<br>
+		<input type="radio" name="often" value="All The Time" required="required">All The Time<br>
 		
-	while(!feof($myfile)){
-		$current = fgets($myfile);
-		switch($current)
-		{
-			case "rarely\n":
-			$rarely++;
-			break;
-			case "sometimes\n":
-			$sometimes++;
-			break;
-			case "often\n":
-			$often++;
-			break;
-			case "All The Time\n":
-			$allTheTime++;
-			break;
-			case "Social Media\n":
-			$socialMedia++;
-			break;
-			case "Playing Games\n":
-			$playingGames++;
-			break;
-			case "Shop\n":
-			$shop++;
-			break;
-			case "Watching Videos\n":
-			$watch++;
-			break;
-			case "Research\n":
-			$research++;
-			break;
-			case "Desktop\n":
-			$desktop++;
-			break;
-			case "Laptop\n":
-			$laptop++;
-			break;
-			case "Tablet\n":
-			$tablet++;
-			break;
-			case "Phone\n":
-			$phone++;
-			break;
-			case "Game Console\n":
-			$console++;
-			break;
-			case "tabs\n":
-			$tabs++;
-			break;
-			case "search\n":
-			$search++;
-			break;
-			case "links\n":
-			$links++;
-			break;
-			default:
-			break;
-		}
-	}
-    fclose($myfile);
+		What do you do most online?<br>
+		<input type="radio" id="social" value="Social Media" name="use" required="required">
+		Use Social Media</input><br>
+		<input type="radio" id="game" value="Playing Games" name="use" required="required">
+		Play Video Games</input><br>
+		<input type="radio" id="shop" value="Shop" name="use" required="required">
+		Shop</input><br>
+		<input type="radio" id="video" value="Watching Videos" name="use" required="required">
+		Watch Vidoes</input><br>
+		<input type="radio" id="research" value="Reasearch" name="use" required="required">
+		Research Information</input><br>
+			   
+		What device are you usually online with?<br>
+		<input type="radio" id="desktop" value="Desktop" name="device" required="required">Desktop Computer</input><br>
+		<input type="radio" id="laptop" value="Laptop" name="device" required="required">Laptop Computer</input><br>
+		<input type="radio" id="tablet" value="Tablet" name="device" required="required">Tablet/I-Pad</input><br>
+		<input type="radio" id="phone" value="Phone" name="device" required="required">Smart Phone</input><br>
+		<input type="radio" id="console" value="Game Console" name="device" required="required">Video Game Console</input><br>
+			   
+		How do you usually navigate a web-page?<br>
+		<input type="radio" name="nav" value="tabs" required="required">The Tabs at the top<br>
+		<input type="radio" name="nav" value="search" required="required">The Search Bar<br>
+		<input type="radio" name="nav" value="links" required="required">The links inside the page body<br>
+		
+		<input type="submit" name="submit"/>
+		
+	</form>
 	
-	echo "<p>How often do you use the internet?</p><ul><li>Rarely:   " . $rarely
-	     . "</li><li>Sometimes:   " . $sometimes . "</li><li>Often:   " . $often
-		 . "</li><li> All The Time:   ". $allTheTime . "</li></ul>";
-	
-	echo "<p>What do you do most online?</p><ul><li>Use Social Media:   " . $socialMedia
-	     . "</li><li>Play Video Games:   " . $playingGames . "</li><li>Shop:   " . $shop
-		 . "</li><li>Watch Videos:   ". $watch . "</li><li>Research: " . $research . "</li></ul>";
-	
-	echo "<p>What device are you usually online with?</p><ul><li>Desktop:   " . $desktop
-	     . "</li><li>Laptop:   " . $laptop . "</li><li>Tablet:   " . $tablet
-		 . "</li><li> Smart Phone:   ". $phone . "</li><li>Video Game Console: " . $console . "</li></ul>";
-
-	echo "<p>How do you usually navigate a web-page?</p><ul><li>Tabs On the top:   " . $tabs . "</li><li>Search Bar: " . $search
-		 . "</li><li>Links in the page:   ". $links . "</li></ul>";	 
-	?>
-	
+	<a href="survey.php">>To Results</a>
 </body>
 </html>
-  
