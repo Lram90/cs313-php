ab5d332ade4bf0a33a118b03cf886422aaa5cc85
Landon Mecham <mec15001@byui.edu>
2017-01-19 17:07:56 -0700

    Writing, second attempt

--- web/survey.php
+++ web/survey.php
@@ -8,9 +8,47 @@
 	<h1>Internet Usage Answers</h1>
 	<h2>CS 313 Week 3 Prove Assignment</h2>
 	<?php
-    $often = $_POST["often"];
-	echo "<h3>How often?<h3>";
-	echo $often;
+	$often = $_POST["often"];
+	$use = $_POST["use"];
+	$device = $_POST["device"];
+	$nav = $_POST["nav"];
+	$answers = survey.txt;
+	$current = "";
+	
+	$current .= $often;
+	$current .= "\n";
+	$current .= $use;
+	$current .= "\n";
+	$current .= $device;
+	$current .= "\n";
+	$current .= $nav;
+	$current .= "\n";
+	
+	if (is_writable($answers)) {
+.
+    if (!$handle = fopen($answers, 'a')) {
+         echo "Cannot open file ($answers)";
+         exit;
+    }
+
+   
+    if (fwrite($handle, $current) === FALSE) {
+        echo "Cannot write $often to file ($answers)";
+        exit;
+    }
+
+    echo "Success, wrote ($somecontent) to file ($filename)";
+
+    fclose($handle);
+
+} else {
+    echo "The file $filename is not writable";
+}
+
+
+	$myfile = fopen("survey.txt", "r") or die("Unable to open file!");
+    echo fread($myfile,filesize("survey.txt"));
+    fclose($myfile);
 	?>
 </body>
 </html>
