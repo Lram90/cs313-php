b59f81274638ecb394ceff103eaa6254381d7b92
Landon Mecham <mec15001@byui.edu>
2017-01-31 23:17:05 -0700

    teach05

---
+++ web/t05Results.php
@@ -0,0 +1,25 @@
+
+
+
+<?php
+		$db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri
+ password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
+ 
+			if(isset($_POST["submit"])){
+				echo "HIT";
+				$book = $_POST["book_search"];
+				$search_query = "SELECT * FROM Scriptures WHERE book= '%".$book."%'"; 
+
+				$search_result = pg_query($search_query); 
+				if (!$search_result) { 
+					echo "Problem with query " . $search_query . "<br/>"; 
+					echo pg_last_error(); 
+					exit(); 
+				} 
+				while($row = pg_fetch_assoc($search_result)) {
+				printf ("<strong>%s %s: %s - </strong> \"%s\" <br>",htmlspecialchars($myrow['book']), htmlspecialchars($myrow['chapter']), htmlspecialchars($myrow['verse']), htmlspecialchars($myrow['content']));
+				}
+				
+			}
+		
+?>
