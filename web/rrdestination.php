41ff2532a06655b1849fe5081eb7c969d9d7d780
Landon Mecham <mec15001@byui.edu>
2017-02-02 20:41:16 -0700

    starting to upload prove_05

--- web/rrdestination.php
+++ web/rrdestination.php
@@ -16,14 +16,19 @@
 <h1>Daily Schedule</h1>
 
 <h3>Enter Destination Address:</h3>
-<input type="text" onchange="display()" />
+<input type="text" name="location" onblur="display()" />
 
  <?php 
 	function display(){
         $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri
 		password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
 
-        $query = "SELECT * FROM Scriptures"; 
+        $query = 'SELECT *
+				FROM public.user u, public.pickup p, public.destination d, public.cost c
+				WHERE u.user_id = p.user_id
+				AND u.user_id = d.user_id
+				AND u.user_id = c.user_id;
+				AND d.destination_location =' . $_GET['location']; 
 
         $result = pg_query($query); 
         if (!$result) { 
@@ -32,8 +37,10 @@
             exit(); 
         } 
 
+		printf('<t><tr><th>User Name</th><th>Pickup Location</th><th>Pickup Time</th><th>Extra Considerations</th><th>Destination</th><th>Estimated Time of Arriaval</th><th>distance</th><th>Total Price</th></tr>');
+		
         while($myrow = pg_fetch_assoc($result)) { 
-            printf ("<strong>%s %s: %s - </strong> \"%s\" <br>",htmlspecialchars($myrow['book']), htmlspecialchars($myrow['chapter']), htmlspecialchars($myrow['verse']), htmlspecialchars($myrow['content']));
+            printf ("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",htmlspecialchars($myrow['user_name']), htmlspecialchars($myrow['pickup_location']), htmlspecialchars($myrow['pickup_time']), htmlspecialchars($myrow['extra_considerations']), htmlspecialchars($myrow['destination_location']), htmlspecialchars($myrow['estimated_arrival']), htmlspecialchars($myrow['distance']) htmlspecialchars($myrow['total']));
         }
 	}		
         ?> 
