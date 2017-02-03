ff813fbbcb73dee7e7463affc72d6b8e95d4f8ae
Landon Mecham <mec15001@byui.edu>
2017-02-02 21:41:45 -0700

    starting to upload prove_05

--- web/rrdestination.php
+++ web/rrdestination.php
@@ -23,7 +23,7 @@
 <?php 
          $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
 
-        $query = 'SELECT * FROM public.user;';
+        $query = "SELECT * FROM public.user";
 
 		echo $query;
         $result = pg_query($query); 
