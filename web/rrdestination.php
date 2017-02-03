3210f8fe773b727d5c51e1d2c0e4a0ab6f0d974e
Landon Mecham <mec15001@byui.edu>
2017-02-03 10:06:57 -0700

    should display only four rows

--- web/rrdestination.php
+++ web/rrdestination.php
@@ -12,7 +12,7 @@
 <body>
 <div class="head"><!--easier to format layout-->
 <img></img><!--get a logo figured out for here-->
-<h1>Daily Schedule</h1>
+<h1>Search by Destination</h1>
 
 <h3>Enter Destination Address:</h3>
 <form id="myForm" method="get" />
@@ -23,6 +23,11 @@
 <?php 
          $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
 
+		 
+		 if(isset($_POST["submit"])){
+			 $query = "SELECT * FROM public.user u, public.pickup p, public.destination d, public.cost c WHERE u.user_id = p.user_id AND u.user_id = d.user_id AND u.user_id = c.user_id AND d.destination_location =" . $_POST['location'];
+		 }
+		 
         $query = "SELECT * FROM public.user u, public.pickup p, public.destination d, public.cost c WHERE u.user_id = p.user_id AND u.user_id = d.user_id AND u.user_id = c.user_id";
 
         $result = pg_query($query); 
