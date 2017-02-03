a8ec9b939ef7b8e540c5e7a910c18e6963b55b0f
Landon Mecham <mec15001@byui.edu>
2017-02-02 21:34:07 -0700

    starting to upload prove_05

--- web/rrdestination.php
+++ web/rrdestination.php
@@ -21,9 +21,9 @@
 </form>
 
 <?php 
-        /* $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
+         $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
 
-        $query = 'SELECT * FROM public.user u, public.pickup p, public.destination d, public.cost c WHERE u.user_id = p.user_id AND u.user_id = d.user_id AND u.user_id = c.user_id;'; 
+        /*$query = 'SELECT * FROM public.user u, public.pickup p, public.destination d, public.cost c WHERE u.user_id = p.user_id AND u.user_id = d.user_id AND u.user_id = c.user_id;'; 
 
         $result = pg_query($query); 
         if (!$result) { 
