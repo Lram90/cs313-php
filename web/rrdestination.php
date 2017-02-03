635e59b47a4171e7714cd926f4741fdf779dd370
Landon Mecham <mec15001@byui.edu>
2017-02-02 20:51:56 -0700

    starting to upload prove_05

--- web/rrdestination.php
+++ web/rrdestination.php
@@ -22,7 +22,7 @@
 </form>
 
  <?php 
-	
+	/*
 	if(isset($_GET["submit"])){
         $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri
 		password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
@@ -48,7 +48,7 @@
         }
 	}		
         ?> 
-
+*/
 </body>
 
 </html>
