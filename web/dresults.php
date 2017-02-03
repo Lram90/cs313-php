4c8ceda97f4c2c3ebf3b662a2c10d14426423ae3
Landon Mecham <mec15001@byui.edu>
2017-02-03 10:58:39 -0700

    gave up, writing thrree more pages

--- web/dresults.php
+++ web/dresults.php
@@ -1,3 +1,15 @@
+<?php
+	session_start();
+?>
+<!doctype html>
+<html>
+
+<head>
+<title>Destination Search Results</title>
+<link rel="stylesheet" type="text/css" href="rrstyle.css">
+</head>
+
+<body>
 <?php 
          $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
 
