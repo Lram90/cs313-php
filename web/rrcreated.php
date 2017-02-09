9b8aa7297589b47fbbcf64b19bde1de8f0038d20
Landon Mecham <mec15001@byui.edu>
2017-02-08 21:53:52 -0700

    hopefully inserts into user table

--- web/rrcreated.php
+++ web/rrcreated.php
@@ -9,10 +9,10 @@
  <?php
  
  
-	 /*$db = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");*/
+	 $db = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
 	 
 	 
-	 $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
+	 /*$db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');*/
 	 
 	 $name = $_POST['newUserName'];
 	 $password = $_POST['code'];
@@ -29,7 +29,27 @@
 	  $preparedStatement->execute(array('user_name' => $name, 'password' => $password));
 	   echo "inserted";
 		
-	//echo "<h1>Account created Successfully</h1><a href='rrlogin.php'>Back to Login</a>";
+	echo "<h1>Account created Successfully</h1><a href='rrlogin.php'>Back to Login</a>";
+	
+	$dQuery = 'SELECT * FROM public.user';
+	
+	$result = pg_query($dQuery); 
+        if (!$result) { 
+            echo "Problem with query " . $dQuery . "<br/>"; 
+            echo pg_last_error(); 
+            exit(); 
+        } 
+		
+		echo "<table><tr><th>User Name</th></tr>";
+		
+		//user_name pickup_location pickup_time destination_location estimated_arrival distance total 
+		
+        while($myrow = pg_fetch_assoc($result)) { 
+          echo "<tr><td>" . $myrow['user_name'] . '</td></tr>';
+        }	
+
+			echo "</table>";
+	
  ?>
  </body>
  </html>
