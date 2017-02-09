fbcc3f1db40a6ac8e63fde885889ed410414ad73
Landon Mecham <mec15001@byui.edu>
2017-02-08 21:40:14 -0700

    hopefully inserts into user table

--- web/rrcreated.php
+++ web/rrcreated.php
@@ -7,7 +7,13 @@
  </head>
  <body>
  <?php
-	 $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
+ 
+ 
+	 $db = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
+	 
+	 
+	 /*$db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');*/
+	 
 	 $name = $_POST['newUserName'];
 	 $password = $_POST['code'];
 
