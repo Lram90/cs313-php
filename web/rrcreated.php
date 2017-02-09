a90f36c7c0434de1c4400c86421cea4f6bc0b509
Landon Mecham <mec15001@byui.edu>
2017-02-08 21:50:36 -0700

    hopefully inserts into user table

--- web/rrcreated.php
+++ web/rrcreated.php
@@ -9,10 +9,10 @@
  <?php
  
  
-	 $db = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
+	 /*$db = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");*/
 	 
 	 
-	 /*$db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');*/
+	 $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
 	 
 	 $name = $_POST['newUserName'];
 	 $password = $_POST['code'];
@@ -28,7 +28,6 @@
 		echo "prepared";
 	  $preparedStatement->execute(array('user_name' => $name, 'password' => $password));
 	   echo "inserted";
-	   //$preparedStatement->execute();
 		
 	//echo "<h1>Account created Successfully</h1><a href='rrlogin.php'>Back to Login</a>";
  ?>
