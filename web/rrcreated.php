134b2207f15af8cefd0e13ecf50922a64962a31f
Landon Mecham <mec15001@byui.edu>
2017-02-09 10:49:38 -0700

    pg_prepping

--- web/rrcreated.php
+++ web/rrcreated.php
@@ -10,10 +10,14 @@
 	
 	$dbh = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
 
+	$name = $_POST['newUserName'];
+	$code = $_POST['code'];
+	
 	$stmt = $dbh->prepare('INSERT INTO public.user(user_name, password) VALUES(:name, :code)');
 	$stmt->execute( array('name' => $name, 'code' => $code) );
 	
-	
+	/*$stmt->bindParam(':name', $name);
+	$stmt->bindParam(':value', $value);*/
 	
 	
 	/* $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
