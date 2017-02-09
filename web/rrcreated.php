f4f70a196acdc9c99867335896b0f2414cb4dd05
Landon Mecham <mec15001@byui.edu>
2017-02-09 10:38:40 -0700

    ABCD

--- web/rrcreated.php
+++ web/rrcreated.php
@@ -7,7 +7,16 @@
  </head>
  <body>
  <?php
-	 $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
+	
+	$dbh = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
+
+	$stmt = $dbh->prepare('INSERT INTO public.user(user_name, password) VALUES(:name, :code)');
+	$stmt->execute( array('name' => $name, 'code' => $code) );
+	
+	
+	
+	
+	/* $db = pg_connect('host=ec2-54-243-38-139.compute-1.amazonaws.com dbname=d89833096k0ivr user=uhieutjjtvpbri password=53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56');
 	 $name = $_POST['newUserName'];
 	 $code = $_POST['code'];
 
@@ -18,19 +27,8 @@
  
 	$result = pg_query($query);
 
-
-	 echo $query;
-	 
-       $preparedStatement = pg_prepare($query);
-
-	   echo "<br>" . $name;
-	   $preparedStatement->bindValue(':name', $name);
-	   $preparedStatement->bindValue(':code', $code);
-	   echo "<br>" . $name. "2";
-	   
-	   //$preparedStatement->execute();
-		
-	//echo "<h1>Account created Successfully</h1><a href='rrlogin.php'>Back to Login</a>";
+		*/
+	echo "<h1>Account created Successfully</h1><a href='rrlogin.php'>Back to Login</a>";
  ?>
  </body>
  </html>
