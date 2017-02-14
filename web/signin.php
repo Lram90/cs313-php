492efcde5f142349eed2c335ceaf1bbc601f169a
Landon Mecham <mec15001@byui.edu>
2017-02-13 18:09:50 -0700

    teach 07 activity

--- web/signin.php
+++ web/signin.php
@@ -1,5 +1,5 @@
 <?php
-
+	session_start();
 	if ($_SERVER["REQUEST_METHOD"] === "POST") {
 	$dbh = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
  
