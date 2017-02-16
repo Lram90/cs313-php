4e9ef41afa15763038b818b0b11ff359d020dbd7
Landon Mecham <mec15001@byui.edu>
2017-02-15 20:14:41 -0700

    better style on home

--- web/rrordered.php
+++ web/rrordered.php
@@ -1,10 +1,11 @@
 <?php
  session_start();
- echo $_SESSION["id"];
  ?>
  
  <!doctype html>
  <head>
+<title>Rexburg Rides</title>
+<link rel="stylesheet" type="text/css" href="rrstyle.css">
  </head>
  <body>
  <?php
@@ -27,10 +28,6 @@
 	$distance = $_POST['distance'];
 	$total = $_POST['total'];
 	
-	
-	echo $name . "<br>" . $origin . "<br>" . $date . "<br>" . $time . "<br>" . $extra . "<br>" . $id . "<br>" . $destination . "<br>" . $duration . "<br>" . $distance . "<br>" . $total . "<br>";
-	
-	
 	$pStmt = $dbh->prepare('INSERT INTO public.pickup(pickup_location, pickup_time, extra_considerations, user_id, pickup_date) VALUES(:origin, :time, :extra, :id, :date)');
 	$pStmt->execute( array('origin' => $origin, 'time' => $time, 'extra' => $extra, 'id' => $id, 'date' => $date) );
 	
@@ -42,7 +39,7 @@
 	$cStmt = $dbh->prepare('INSERT INTO public.cost(distance, total, user_id) VALUES(:distance, :total, :id)');
 	$cStmt->execute( array('distance' => $distance, 'total' => $total, ':id' => $id) );
 	
-	echo "<h1>Order Successful!</h1><a href='rrlogin.php'>Back to Login</a><br /> <br /> <h2>YOUR RIDES</h2>";
+	echo "<h1>Order Successful!</h1><a href='rrlogin.php'>Back to Login</a><br /> <br />";
  ?>
  </body>
  </html>
