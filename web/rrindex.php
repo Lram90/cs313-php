a0663b3edc9819fac6a7f3b59ec9170c2c3e0c6d
Landon Mecham <mec15001@byui.edu>
2017-02-17 13:13:11 -0700

    styling other pages

--- web/rrindex.php
+++ web/rrindex.php
@@ -1,29 +1,5 @@
 <?php
 	session_start();
-	$_SESSION['user'] = $_POST['uname'];
-	$_SESSION['pass'] = $_POST['psw'];
-	
-
-	
-	$dbh = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
- 
-	$name = $_SESSION['user'];
-	$code = $_SESSION['pass'];
-	
-
- 
-	$testQuery = $dbh->prepare('SELECT * FROM public.user WHERE user_name = :name AND password = :code');
-
-	$testQuery->execute( array('name' => $name, 'code' => $code) );
-
-	$row = $testQuery->fetch();
-	$_SESSION['id'] = $row['user_id'];
-	
-	if ($_SESSION['id'] === NULL) {
-		$message = "Your User Name or Password do not match";
-		echo "<script type='text/javascript'>alert('$message');</script>";
-		header('Location: https://stormy-spire-65023.herokuapp.com/rrwarning.php', true);
-	}
 ?>
 
 <!doctype html>
