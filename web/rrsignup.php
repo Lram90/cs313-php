3461d91f1a9b71bcea769824981e7cdd21fe6360
Landon Mecham <mec15001@byui.edu>
2017-02-17 11:17:02 -0700

    styling other pages

--- web/rrsignup.php
+++ web/rrsignup.php
@@ -7,11 +7,13 @@
 	
 		$hash = password_hash($password, PASSWORD_DEFAULT);
 	
+		$temp = 1;
+	
 		$dbh = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
 
 	
 		$stmt = $dbh->prepare('INSERT INTO public.user(user_name, password, user_type) VALUES(:name, :hash, :type)');
-		$stmt->execute( array('name' => $_POST['user'], 'hash' => $hash, 'type' => 1) );
+		$stmt->execute( array('name' => $_POST['user'], 'hash' => $hash, 'type' => $temp) );
 		
 		header('Location: rrlogin.php', true);
 	}
