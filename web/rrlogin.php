eb37e59f60fe185709ceababde28cdfb2404083f
Landon Mecham <mec15001@byui.edu>
2017-02-17 10:26:06 -0700

    styling other pages

--- web/rrlogin.php
+++ web/rrlogin.php
@@ -5,50 +5,12 @@
 
 <!DOCTYPE html>
 <html>
+<head>
+<title>Rexburg Rides</title>
 <style>
-form {
-    border: 3px solid #f1f1f1;
-}
-
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
-
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-}
-
-
-.container {
-    padding: 16px;
-}
-
-span.psw {
-    float: right;
-    padding-top: 16px;
-}
-
-@media screen and (max-width: 300px) {
-    span.psw {
-       display: block;
-       float: none;
-    }
-    .cancelbtn {
-       width: 100%;
-    }
-}
+<link rel="stylesheet" type="text/css" href="rrstyle.css">
 </style>
+</head>
 <body>
 
 <h2>Login Form</h2>
--- web/rrsignup.php
+++ web/rrsignup.php
@@ -1,65 +1,29 @@
 <?php
  session_start();
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+		$password = $_POST['password'];
+	
+		$hash = password_hash($password, PASSWORD_DEFAULT);
+	
+		$dbh = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
+
+	
+		$stmt = $dbh->prepare('INSERT INTO public.user(user_name, password) VALUES(:name, :hash)');
+		$stmt->execute( array('name' => $_POST['user'], 'hash' => $hash) );
+		
+		header('Location: rrlogin.php', true);
  ?>
 
 <!doctype HTML>
 <head>
 <style>
-/* Full-width input fields */
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
-
-/* Set a style for all buttons */
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-}
-
-/* Extra styles for the cancel button */
-.cancelbtn {
-    padding: 14px 20px;
-    background-color: #f44336;
-}
-
-/* Float cancel and signup buttons and add an equal width */
-.cancelbtn,.signupbtn {
-    float: left;
-    width: 50%;
-}
-
-/* Add padding to container elements */
-.container {
-    padding: 16px;
-}
-
-/* Clear floats */
-.clearfix::after {
-    content: "";
-    clear: both;
-    display: table;
-}
-
-/* Change styles for cancel button and signup button on extra small screens */
-@media screen and (max-width: 300px) {
-    .cancelbtn, .signupbtn {
-        width: 100%;
-    }
-}
+<link rel="stylesheet" type="text/css" href="rrstyle.css">
 </style>
 </head>
 <body>
-<form action="rrcreated.php" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
   <div class="container">
     <label><b>UserName</b></label>
     <input type="text" placeholder="Enter UserName" name="newUserName" required>
