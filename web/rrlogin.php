f7cca1ff93dc049e2b6cb447dd017779679d2838
Landon Mecham <mec15001@byui.edu>
2017-02-17 13:03:23 -0700

    styling other pages

--- web/rrlogin.php
+++ web/rrlogin.php
@@ -1,6 +1,32 @@
 
 <?php
 	session_start();
+	
+	if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	$dbh = new PDO("pgsql:host=ec2-54-243-38-139.compute-1.amazonaws.com;port=5432;dbname=d89833096k0ivr", "uhieutjjtvpbri", "53f15317bc3fba7ca9c92f06895fa510ae3cefe2d63972966a0c2140559b6b56");
+ 
+	$name = $_POST['uname'];
+	$pass = $_POST['psw'];
+	
+
+ 
+	$testQuery = $dbh->prepare('SELECT * FROM public.user WHERE user_name = :name');
+
+	$testQuery->execute( array('name' => $name) );
+
+	$row = $testQuery->fetch();
+	$hash = $row['password'];
+	
+	if (password_verify($pass, $hash)) {
+		$_SESSION['username'] = $_POST['username'];
+		header('Location: rrindex.php', true);
+	}
+	else{
+		$message = "Your User Name or Password do not match";
+		echo "<script type='text/javascript'>alert('$message');</script>";
+	}
+	
+	}
 ?>
 
 <!DOCTYPE html>
